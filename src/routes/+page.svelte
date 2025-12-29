f4cf7a5c0706f332d182aaf9ea0318756feeccc8
fawlty-code <175266429+fawlty-code@users.noreply.github.com>
2025-12-29 23:58:27 +0530

    create page layour

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,9 +11,9 @@
 
 <div id="header" class="bg-off-white"></div>
 <div class="bg-light-purple min-h-200 flex flex-row">
-    <div class="flex-1 shrink-2 flex flex-col">
-        <span class="text-6xl" id="title">happy six months, spicy loaded legend</span>
-        <span></span>
+    <div class="flex-1 shrink-2 flex flex-row">
+        <span class="text-6xl fill-off-white" id="title">happy six months, spicy loaded legend</span>
+        <span class="text-4xl fill-off-white" id="title">writing this while listening to phir le aaya dil and yun hi back to back. i cannot believe it's really been 6 months. i used to imagine us hitting this milestone when we first started talking. and now that we're here it feels like we've known each other for lifetimes. and 6 months down the line </span>
     </div>
     <div class="flex-2">
 
