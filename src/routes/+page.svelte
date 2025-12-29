fc85fb2f58ec5e845b198589ec624c9d1f3a6206
fawlty-code <175266429+fawlty-code@users.noreply.github.com>
2025-12-29 18:19:58 +0530

    Update layout and dependencies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import icon from '$lib/assets/favicon.png';
+</script>
+
+<style>
+    #title {
+        font-family: Margin;
+        text-align: center;
+    }
+</style>
+
+<div id="header" class="bg-off-white"></div>
+<div class="bg-light-purple min-h-200 flex flex-row">
+    <div class="flex-1 shrink-2 flex flex-col">
+        <span class="text-6xl" id="title">happy six months, spicy loaded legend</span>
+        <span></span>
+    </div>
+    <div class="flex-2">
+
+    </div>
+</div>
